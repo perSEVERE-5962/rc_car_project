3060436212a3570d52febbc4fff77618636d4b33
david-lemasurier <david.lemasurier@gmail.com>
2020-07-25 19:33:05 -0400

    added sensor template

--- template/python/README.md
+++ template/python/README.md
@@ -3,5 +3,5 @@
 The templates in this folder are meant to be examples that can be used when creating the source for the project
 
 Templates:
-1. sensor_library_template.py - use this template when creating a new sensor library
-2. command_template.py - use this template to when creating a new command
+1. sensorTemplate.py - use this template when creating a new sensor library
+2. commandTemplate.py - use this template to when creating a new command
---
+++ template/python/sensorTemplate.py
@@ -0,0 +1,57 @@
+# Make an empty file called __init__.py in the same directory as the sensor library file that you are creating
+#     You could also copy the one from template/python folder
+#
+# That will signify to Python that it's "ok to import from this directory"
+#
+# Then, in your main program file you can import your class
+#     For example, if your file is called hcsr04.py and the class is called Hcsr04
+#     you would add the following to your main program file
+#         from hcsr04 import Hcsr04
+#
+
+
+# import any of the required libraries for the sensor
+
+# include the networktablesinstance
+from networktables import NetworkTablesInstance
+
+# create the class
+# see https://www.programiz.com/python-programming/class for a tutorial
+
+
+class SensorTemplate:
+    "Put a description of your class here in quotes"
+
+    # setup network tables
+    ntinst = NetworkTablesInstance.getDefault()
+    ntinst.startClient("put ip address of the server here")
+    # change "sensor" to be the name of your sensor
+    table = ntinst.getTable("sensor")
+    # change the following to match the information you want to store for your sensor
+    sensor_value = table.getEntry("sensor_value")
+    sensor_value_2 = table.getEntry("sensor_value_2")
+    # add as many entries as you need
+
+    # create the constructor to initialize the class
+    # you can specify as many parameters as needed, with or without defaults
+    # see https://www.programiz.com/python-programming/function-argument for a tutorial
+    def __init__(self, value1=0, value2=0):
+        self.sensor_value = value1
+        self.sensor_value_2 = value2
+
+    def reset(self):
+        # TODO: add you reset logic here
+        print("sensor reset")
+
+    def get_sensor_value(self):
+        return sensor_value
+
+    def get_sensor_value_2(self):
+        return sensor_value_2
+
+    while True:
+        # TODO: add your sensor logic here
+
+        # store the values in the network table
+        sensor_value.setValue(sensor_value)
+        sensor_value_2.setValue(sensor_value_2)
